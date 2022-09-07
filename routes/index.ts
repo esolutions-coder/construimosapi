da6bf1a67d91963b024ec89b39649608e6532856
Miguel <migueltp34@gmail.com>
2022-09-07 18:39:04 -0500

    fixed index

--- routes/index.ts
+++ routes/index.ts
@@ -1,16 +1,9 @@
 import express from "express";
-import apus from "../models/apus";
-import sevilla_apu from "../utils/apus.json"
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', async (req, res) => {
-  for (let i = 0; i < sevilla_apu.length; i++) {
-    const currentApu = sevilla_apu[i]
-    const newApu = new apus({ ...currentApu })
-    await newApu.save()
-  }
-  res.json({ response: "Completado" })
+  res.json({ response: "Bienvenido a CONSTRUIMOS API" })
 });
 
 export default router
